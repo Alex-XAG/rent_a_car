89ee92ad393e0c1c4c8795568d0da5ec56cf8956
Alex-XAG <109016896+Alex-XAG@users.noreply.github.com>
2023-10-11 17:54:39 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-import { StateContext } from 'Context/StateContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/rent_a_car">
-      <StateContext>
-        <App />
-      </StateContext>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
